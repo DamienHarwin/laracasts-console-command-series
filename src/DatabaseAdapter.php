66ab4a6600ba9d4fa6afded03199e248c02e88a8
Damien Harwin <damien.harwin@mayden.co.uk>
2016-06-01 18:03:28 +0100

    WIP
    WIP 4:01 in

---
+++ src/DatabaseAdapter.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Acme;
+
+
+class DatabaseAdapter
+{
+
+}
---
+++ src/ShowCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Acme;
+
+use Symfony\Component\Console\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ShowCommand extends Command
+{
+
+    /**
+     * @var DatabaseAdapter
+     */
+    private $database;
+
+    public function __construct(DatabaseAdapter $database)
+    {
+
+        $this->database = $database;
+        parent::__construct();
+    }
+
+    public function configure()
+    {
+        $this->setName('show')
+             ->setDescription('Show all tasks.');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->showTasks($output);
+    }
+
+    private function showTasks(OutputInterface $output)
+    {
+        $tasks = $this->database->fetchAll('tasks');
+        $table = new Table($output);
+        $table->setHeaders(['Id', 'Description'])
+            ->setRows($tasks)
+            ->render();
+
+    }
+}
